711dbddcc8e3d8267833bb77b464df7d3e02be58
blazekradomir <blazekradomir@gmail.com>
2025-06-29 09:33:31 +0200

    29_6_25

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -14,13 +14,21 @@ def emotion_detector(text_to_analyze):
 
     # Make a POST request to the API with the payload and headers
     response = requests.post(url, json=myobj, headers=header)
+    
+    # Error handling error_code == 400
+    if response.status_code == 400:
+        # We get the list of emotions
+        fallback_keys = ["joy", "sadness", "anger", "fear", "disgust"]
+        return {k: None for k in fallback_keys + ["dominant_emotion"]}
 
     #Conver text to json
     formatted_response = json.loads(response.text)
 
     # Extract the emotion from the response
     emotion = formatted_response["emotionPredictions"][0]["emotion"]
-    emotion["dominant_emotion"] = max(emotion.values())
+    # emotion['dominant_emotion'] = max(emotion.values())
+    emotion["dominant_emotion"] = max(emotion, key = emotion.get)
+    # response["dominant_emotion"] = max(response, key=response.get)
 
     # Return the result as a text
     return emotion
--- server.py
+++ server.py
@@ -1,3 +1,6 @@
+'''
+This server is for testing my knowlage
+'''
 # Import Flask, render_template, request from the flask pramework package :
 from flask import Flask, render_template, request
 # Import the emotion_detector function from the package created:
@@ -9,15 +12,34 @@ app = Flask('Emotion Detector')
 
 @app.route("/emotionDetector")
 def emotion_analyzer():
+    '''
+    This function retuns emotion analysis
+    '''
     # Retrieve the text to analyze from the request arguments
     text_to_analyze = request.args.get('textToAnalyze')
 
     # Pass the text to the sentiment_analyzer function and store the response
     response = emotion_detector(text_to_analyze)
 
-    # Return a formatted string with the sentiment label and score
-    return response
+    # Test if some key (in this key "joy") is "None"
+    if response.get("joy") is None:
+        return "Invalid text! Please try again!"
 
+    # Return a formatted string with the prescripted text
+    #emotion_list = [f"{name}: {score}" for name, score in response.items()
+    # if name != "dominant_emotion"]
+    emotion_list = [
+    f"{name}: {score}"
+    for name, score in response.items()
+    if name != "dominant_emotion"
+    ]
+    emotion_str = ", ".join(emotion_list)
+    dominant = response.get("dominant_emotion", "not available")
+
+    return (
+        f"For the given statement, the system detected the following emotions: "
+        f"{emotion_str}. The dominant emotion is: {dominant}."
+    )
 @app.route("/")
 def render_index_page():
     ''' 
